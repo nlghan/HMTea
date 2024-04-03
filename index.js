807cd0ac1cabe13b69589f206f288236c223641f
giahann <giahan05092003@gmail.com>
2024-04-03 12:35:05 +0700

    sửa lại tên các img, filde data và có 2 màn hình login, register nhưng chưa có chuyển trang

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import Login from './src/screens/Login';
 import {name as appName} from './app.json';
+import Register from './src/screens/Register';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Register);
